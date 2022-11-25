fec652181679343fd7376e9c1875969c88a35e36
JosefinoRamirez <ramirezvindasjose@gmail.com>
2022-11-25 11:54:08 -0600

    Fix v1

--- src/pages/comps/footer.jsx
+++ src/pages/comps/footer.jsx
@@ -21,40 +21,40 @@ const Footer = (props) => {
 
     return (
       <footer className="text-white" style={{ backgroundColor: '#000000' }}>
-        <section style={{ backgroundColor: '#132523' }} className={"pb-28 flex flex-col items-center px-8 md:px-0 " + props.footerForm}>
+        <section style={{ backgroundColor: '#132523' }} className={"pb-28 flex flex-col items-center px-8 xl:px-0 " + props.footerForm}>
             <div className="flex flex-col items-center max-w-xl text-white pt-36">
               <p className="text-xl mb-4">Ready to get started?</p>
-              <h1 className="text-5xl md:text-6xl text-left md:text-center mb-4">Let’s start talking about your project.</h1>
-              <span className="flex flex-col w-full md:w-3/4 mb-3">
+              <h1 className="text-5xl xl:text-6xl text-left xl:text-center mb-4">Let’s start talking about your project.</h1>
+              <span className="flex flex-col w-full xl:w-3/4 mb-3">
                 <label>Email Address</label>
                 <InputText value={usrEmail} onChange={(e) => setUsrEmail(e.target.value)} placeholder="Enter you Email Address" className="rounded-lg" />
               </span>
-              <span className="flex flex-col w-full md:w-3/4 mb-6">
+              <span className="flex flex-col w-full xl:w-3/4 mb-6">
                 <label>What services are you interested in?</label>
                 <MultiSelect value={usrService} options={citySelectItems} onChange={(e) => setUsrService(e.value)} placeholder="Select all that apply" className="rounded-lg" />
               </span>
               <Button label="Get in touch >" className="rounded-lg w-3/4" style={{ backgroundColor: '#103737', borderColor: '#ffffff' }} />
             </div>
         </section>
-        <div className="container m-auto flex flex-col md:flex-row items-start justify-between border-b-black md:border-b-white border-b-2 py-24 px-8 md:px-0 pb-8 md:pb-24">
+        <div className="container m-auto flex flex-col xl:flex-row items-start justify-between border-b-black xl:border-b-white border-b-2 py-24 px-8 xl:px-0 pb-8 xl:pb-24">
           <Link>
-            <img src="./LogoWhite.png" alt="Logo Coto" className="w-20 mr-14 mb-7 md:mb-0"/>
+            <img src="./LogoWhite.png" alt="Logo Coto" className="w-20 mr-14 mb-7 xl:mb-0"/>
           </Link>
-          <section className="flex flex-col mb-7 md:mb-0">
+          <section className="flex flex-col mb-7 xl:mb-0">
             <h6>Site Map</h6>
             <MenuItems/>
           </section>
-          <section className="flex flex-col mb-16 md:mb-0">
+          <section className="flex flex-col mb-16 xl:mb-0">
             <h6>Contact Us</h6>
             <p><i className="pi pi-phone"></i> (+506) 8888-0032</p>
             <p><i className="pi pi-envelope"></i> contact@cotocompany.com</p>
           </section>
-          <section className="flex flex-col md:w-96 border-t-white border-t-2 md:border-t-0 pt-8 md:pt-0">
+          <section className="flex flex-col xl:w-96 border-t-white border-t-2 xl:border-t-0 pt-8 xl:pt-0">
             <h6>Visit Us</h6>
             <p>The Dome Plaza next to ICE / Kolbi and Coopealianza Osa, Uvita, 60504, Costa Rica.</p>
           </section>
         </div>
-        <div className="container m-auto flex flex-col md:flex-row justify-between px-8 md:px-0 pt-8  pb-8 md:pb-24">
+        <div className="container m-auto flex flex-col xl:flex-row justify-between px-8 xl:px-0 pt-8  pb-8 xl:pb-24">
           <section className="flex w-32 justify-between mb-8">
             <p>Follow Us</p>
             <a href="/"><li className="pi pi-facebook"></li></a>
--- src/pages/comps/menu.jsx
+++ src/pages/comps/menu.jsx
@@ -11,19 +11,33 @@ const Menu = () => {
 
     return (
       <header className="py-4 container mx-auto">
-        <nav className="flex flex-row items-center justify-between md:justify-start px-8 md:px-0">
+        <nav className="flex flex-row items-center justify-between xl:justify-start px-8 xl:px-0">
          <Link to={`/`}>
             <img src="./Logo.png" alt="Logo Coto" className="w-20 mr-14"/>
          </Link>
-          <div className="hidden md:block">
+          <div className="hidden xl:block">
             <MenuItems/>
           </div>
-          <img src='./HamMenu.png' alt='Mobile Menu' className='w-5 block md:hidden' onClick={() => setVisibleRight(true)}/>
+          <img src='./HamMenu.png' alt='Mobile Menu' className='w-5 block xl:hidden' onClick={() => setVisibleRight(true)}/>
         </nav>
         <Sidebar visible={visibleRight} position="right" onHide={() => setVisibleRight(false)}>
-            <div className="flex flex-col">
-              <MenuItems/>
+            <div className="flex flex-col pt-16 text-5xl text-black">
+              <img src="./LogoIcon.png" alt="Logo Coto" className="w-14 mb-9"/>
+              <Link to={'/'} className='mb-8'><h2>Home</h2></Link>
+              <div className='flex flex-col mb-24'>
+                <MenuItems
+                  className='mb-8'
+                />
+              </div>
             </div>
+            <div className="container m-auto flex flex-col">
+                <section className="flex w-32 justify-between mb-8">
+                  <p>Follow Us</p>
+                  <a href="/"><li className="pi pi-facebook"></li></a>
+                  <a href="/"><li className="pi pi-linkedin"></li></a>
+                </section>
+                <p className='text-gray-400'><small>© Coto Company, Costa Rica, 2022. All rights reserved.  Powered by Milk Design Studio. </small></p>
+              </div>
         </Sidebar>
       </header>
     );
