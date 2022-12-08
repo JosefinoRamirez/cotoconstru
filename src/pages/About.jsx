eb84ad2fda618e0c26ca11af32ad394082bb21ad
JosefinoRamirez <ramirezvindasjose@gmail.com>
2022-12-08 10:42:36 -0600

    New Slider

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,10 @@
 import { useEffect } from 'react';
 
-import { Galleria } from 'primereact/galleria';
+import { Carousel } from 'primereact/carousel';
 
 import Menu from "./comps/menu";
 import Footer from './comps/footer';
 import Looking from "./comps/lookingCarousel";
-
-const Card = (props) => {
-    return (
-        <div className="pt-24 px-16 pb-32 bg-gray-50 border-2 mb-16" style={{ width: '49%' }}>
-            <img src={props.src} alt={props.alt} className='w-16 mb-6' />
-            <h2 className={'text-3xl mb-2 ' + props.h2w }>{props.title}</h2>
-            <p>{props.description}</p>
-        </div>
-    );
-}
 
 const TeamCard = (props) => {
     return (
@@ -41,7 +31,7 @@ const TeamCard = (props) => {
 
 const itemTemplate = (item) => {
     return (
-        <div className="pt-16 px-5 pb-20 bg-gray-50 border-2 mb-16 w-full flex flex-col items-center text-center">
+        <div className="pt-16 px-5 pb-20 bg-gray-50 border-2 mb-16 w-11/12 h-96 flex flex-col items-center text-center">
             <img className='w-16 mb-7' src={item.imageSrc} alt={item.alt} />
             <h2 className='text-2xl mb-2'>{item.caseTitle}</h2>
             <p>{item.smallDesc}</p>
@@ -58,6 +48,29 @@ const About = () => {
         { 'imageSrc': './Icon3.png', 'alt': 'Project Management', 'caseTitle': 'Project Management', 'smallDesc': 'In the course of a project, a number of different elements need to be considered. That’s why our Project Managers will focus on good team coordination and solid collaboration between all the agents from the time a project is initiated until it is complete.' },
         { 'imageSrc': './Icon4.png', 'alt': 'Keys In Hand', 'caseTitle': 'Keys In Hand', 'smallDesc': 'We offer a comprehensive, integrated service that encompasses all project stages with a strong emphasis on quality, streamlining production and minimizing risks for timely completion.' },
     ]
+
+    const responsiveOptions = [
+        {
+            breakpoint: '1440px',
+            numVisible: 2,
+            numScroll: 2
+        },
+        {
+            breakpoint: '1024px',
+            numVisible: 2,
+            numScroll: 2
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 2,
+            numScroll: 2
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];
 
     useEffect(() => {
         window.scrollTo(0,0)
@@ -77,46 +90,14 @@ const About = () => {
                 <h1 className="text-5xl xl:text-6xl text-center mb-6 max-w-4xl">We offer everything you’ll need, in one place.</h1>
                 <p className="max-w-4xl text-center">From the initial concept to project closeout, we assist our clients in facilitating and coordinating the project process to mitigate complex problems. Our ability to adapt to breathtaking beauty around us has enabled us to deliver projects that exceed expectations for more than 15 years.</p>
             </section>
-            <section className="hidden container mx-auto xl:flex flex-row flex-wrap justify-between max-w-7xl mb-24">
-                <Card
-                    src='./Icon1.png'
-                    alt='Due Diligence'
-                    title='Due Diligence'
-                    h2w='w-32'
-                    description='This service provides assurance to owners and funders that the project is feasible by assisting with the identification and analysis of risks and provide recommendations on how to mitigate them through construction planning.'
-                />
-                <Card
-                    src='./Icon2.png'
-                    alt='Profesional Construction & Architecture'
-                    title='Profesional Construction & Architecture'
-                    h2w=''
-                    description='Our commitment to quality, safety, innovation, and technical expertise provides our clients and industry partners with particular assurance. In addition, our expertise, local market knowledge, and close ties to supply chains all contribute to our success.'
-                />
-                <Card
-                    src='./Icon3.png'
-                    alt='Project Management'
-                    title='Project Management'
-                    h2w='w-32'
-                    description='In the course of a project, a number of different elements need to be considered. That’s why our Project Managers will focus on good team coordination and solid collaboration between all the agents from the time a project is initiated until it is complete.'
-                />
-                <Card
-                    src='./Icon4.png'
-                    alt='Keys In Hand'
-                    title='Keys In Hand'
-                    h2w='w-32'
-                    description='We offer a comprehensive, integrated service that encompasses all project stages with a strong emphasis on quality, streamlining production and minimizing risks for timely completion.'
-                />
-            </section>
-            <Galleria 
+            <Carousel 
                 value={images} 
-                numVisible={1} 
-                circular 
-                showItemNavigators 
-                showThumbnails={false} 
-                showItemNavigatorsOnHover 
-                showIndicators 
-                item={itemTemplate} 
-                className='xl:hidden px-8'
+                responsiveOptions={responsiveOptions}
+                itemTemplate={itemTemplate}
+                showNavigators
+                showIndicators={false}
+                className='cotoAboutCarousel'
+                circular
             />
             <section className="container mx-auto pb-32 px-8 xl:px-0">
                 <img src="./Goal.png" alt="Goal" className="w-full object-cover mb-24"/>
